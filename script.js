0c774b510a2354be8fc6c8f85944f4034916a6f0
Harika sri Gantlapu <harikasrigantlapu@gmail.com>
2026-06-06 22:11:01 +0530

    Initial Portfolio Website Upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typing",{
+strings:[
+"Web Developer",
+"Salesforce Enthusiast",
+"Cloud Learner",
+"Future Software Engineer"
+],
+typeSpeed:80,
+backSpeed:50,
+loop:true
+});
